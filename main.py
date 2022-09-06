a040767bd8797732f75063f962274d5cc09c389d
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-06 01:27:54 -0300

    Update main.py
    criação do corpo da snake

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from turtle  import Screen, Turtle
+from re import X
+from turtle  import Screen, Turtle, color, penup
 
 screen = Screen()
 screen.setup(width= 600, height= 600)   #tamanho do screnn em pixels
 screen.bgcolor("black")      #altera a cor da tela
 screen.title("Snake Game")   #adiciona um ítulo a tela
+
+lista_corpo = []
+lista_posicao = [0,-20,-40]
+
+for parte_corpo in range(0,3):
+    nova_parte = Turtle(shape="square")
+    nova_parte.color("white")
+    nova_parte.penup()
+    nova_parte.goto(x=lista_posicao[parte_corpo], y=0)
+    lista_corpo.append(nova_parte)
+
+
+
 
 
 
