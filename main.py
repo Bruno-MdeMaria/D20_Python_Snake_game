4a5b091f88f9e71a5d8c626d9579265377e8cf25
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-06 03:04:10 -0300

    Update main.py
    fazendo com que a movmentacao do novo objeto seja a ultima posicao do primeiro opbjeto

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ screen.tracer(0)             #desligar o rastreador colocando 0 para mostrar uma
 lista_corpo = []
 lista_posicao = [(0,0), (-20, 0), (-40, 0)]   #tupla com a posição inicial(goto) x= e y=
 
-for parte_corpo in lista_posicao:               #loop para cirar 3 partes do corpo usando a lista_posicao
+for posicao in lista_posicao:               #loop para cirar 3 partes do corpo usando a lista_posicao
     nova_parte = Turtle(shape="square")         #altera o formato da tataruga para quadrado(square)
     nova_parte.color("white")                   #altera a cor para branco
     nova_parte.penup()                          #ergue a caneta e retira o rastro quando se movimenta
     nova_parte.speed(1)                         #movimento mais lento(1,3,6,10,0) sendo o 0 super rápido.
-    nova_parte.goto(parte_corpo)                #posicao inicial recebe a posicao da lista_posicao conforme o loop
+    nova_parte.goto(posicao)                #posicao inicial recebe a posicao da lista_posicao conforme o loop
     lista_corpo.append(nova_parte)              #lista de corpo recebe tres partes de corpo.
     
 
@@ -24,9 +24,14 @@ for parte_corpo in lista_posicao:               #loop para cirar 3 partes do cor
 game_iniciado = True
 while game_iniciado:
     screen.update()     #inicia o screen para fazer o movimento da snake
-    time.sleep(0.1)     #necessário importar o modo TIME. Esta velocidade definida mostra os quadros mais lentamente.
-    for parte_corpo in lista_corpo:
-        parte_corpo.forward(20)
+    time.sleep(0.1)     #necessário importar o modo TIME. Isto acrescenta um atraso de 0.1 segundo entre cada parte do corpo se mover.
+    for numero_parte in range(len(lista_corpo) -1, 0,-1):
+        nova_posx = lista_corpo[numero_parte -1].xcor()
+        nova_posy = lista_corpo[numero_parte -1].ycor()
+        lista_corpo[numero_parte].goto(nova_posx, nova_posy)
+        
+posicao[0].forward(20)
+
 
 
 
