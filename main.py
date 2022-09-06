7dba50dc55310a0743c2e3f963e5cb719a81c017
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-06 01:09:53 -0300

    Create main.py
    iniciando a configuração da tela

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle  import Screen, Turtle
+
+screen = Screen()
+screen.setup(width= 600, height= 600)   #tamanho do screnn em pixels
+screen.bgcolor("black")      #altera a cor da tela
+screen.title("Snake Game")   #adiciona um ítulo a tela
+
+
+
+
+screen.exitonclick()    #fechar a tela somente após um clique
+
+
+
