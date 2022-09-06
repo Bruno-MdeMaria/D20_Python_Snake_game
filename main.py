91dcecfa2078a7c3760dfc50fd6eade52e67e40d
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-06 01:41:00 -0300

    Update main.py
    incluido comentários

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ screen.bgcolor("black")      #altera a cor da tela
 screen.title("Snake Game")   #adiciona um ítulo a tela
 
 lista_corpo = []
-lista_posicao = [(0,0), (-20, 0), (-40, 0)]
+lista_posicao = [(0,0), (-20, 0), (-40, 0)]   #tupla com a posição inicial(goto) x= e y=
 
-for parte_corpo in lista_posicao:
-    nova_parte = Turtle(shape="square")
-    nova_parte.color("white")
-    nova_parte.penup()
-    nova_parte.goto(parte_corpo)
-    lista_corpo.append(nova_parte)
+for parte_corpo in lista_posicao:               #loop para cirar 3 partes do corpo usando a lista_posicao
+    nova_parte = Turtle(shape="square")         #altera o formato da tataruga para quadrado(square)
+    nova_parte.color("white")                   #altera a cor para branco
+    nova_parte.penup()                          #retira o rastro quando movimenta
+    nova_parte.goto(parte_corpo)                #posicao inicial recebe a posicao da lista_posicao conforme o loop
+    lista_corpo.append(nova_parte)              #lista de corpo recebe tres partes de corpo.
+
+    
 
 
 
