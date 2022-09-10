ee4f140ce3f45707a4913016cee9d6285247a8bb
Pricila santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-09-10 08:46:34 -0300

    herança python para criar food
    criado a classe food utilizando a classe pai turtle

--- food.py
+++ food.py
@@ -1,6 +1,13 @@
 from turtle import Turtle
 import random
 
-class food(Turtle):
-    def __init__(self):
-        super().__init__()
+#CRIANDO A COMIDA DA SERPENTE UTILIZANDO HERANÇA DE CLASSE:
+
+class food(Turtle):                  #CRIADO A CLASSE FOOD UTILIZANDO A CLASSE PAI: Turtle.
+    def __init__(self):              #INICADO OS ATRIBUTOS DA COMIDA __INIT__   
+        super().__init__()           #importado da classe pai(super classe)
+        self.shape("circle")         #utlizando os atributos da classe pai(Turtle) como .shape, .penup,.color
+        self.penup()
+        self.shapesize()
+        self.color("blue")
+        
