802b4bb66f5b9a5ecec2f232fe3eaf5690402ad2
Pricila santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-09-10 11:15:57 -0300

    Update scoreboard.py
    Primeira tentativa registrar placar

--- scoreboard.py
+++ scoreboard.py
@@ -4,4 +4,11 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.write("Pontuação = ", move=False, align="center", font= ('Arial', 8, "normal"))
+        self.pontuar()
+
+    def pontuar(self):
+        placar = 0
+        self.pontuar_placar = placar + 1
+
+
 
