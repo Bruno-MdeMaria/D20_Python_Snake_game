bb608b078d93921537d08d5f6869f957c5dc0199
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-24 08:20:13 -0300

    Update scoreboard.py
    retirado o metodo game over para fazer um novo como reset.
    tentativa de colocar registro de record de pontuação

--- scoreboard.py
+++ scoreboard.py
@@ -10,16 +10,20 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()     #método da Turtle faz desaparecer a seta ou qualquer outro formato da tartaruga.
         self.goto(0,320)
+        self.recorde = 0
         self.pontos = 0
         self.update_scoreboard() #método criado abaixo:
         
         
     def update_scoreboard(self):         #este método vai atualizar a pontuação, pois dentro dos atributos ele nã funiona corretamente.
         self.write(f"Pontos= {self.pontos} ", move=False, align=ALIGN, font= FONT)
+    
+    def reset(self):
+        pass
 
-    def game_over(self):                 #informar ao jogador quando o jogo acabou
-        self.goto(0,0)                   #aparecer no centro da tela e não no topo como no atributo inicial
-        self.write("GAME OVER", move=False, align=ALIGN, font= ("Courier", 20 , "bold"))
+    # def game_over(self):                 #informar ao jogador quando o jogo acabou
+    #     self.goto(0,0)                   #aparecer no centro da tela e não no topo como no atributo inicial
+    #     self.write("GAME OVER", move=False, align=ALIGN, font= ("Courier", 20 , "bold"))
         
     def pontuar(self):
         self.pontos += 1     #aumenta um ponto toda vez que o método é chamado.
